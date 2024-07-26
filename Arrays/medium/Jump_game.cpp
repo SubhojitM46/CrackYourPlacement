e190fac427795c8f71a54b1e3f7a5a1810489c4d
SubhojitM46 <sjnotes123@gmail.com>
2024-07-26 22:52:43 +0530

    jump game

--- Arrays/medium/Jump_game.cpp
+++ Arrays/medium/Jump_game.cpp
@@ -44,36 +44,3 @@ public:
     }
 };
 
-class Solution {
-public:
-    bool canJump(vector<int>& nums) {
-        // if(nums[0]==0 && nums.size()==1)
-        // return true;
-
-        // if(nums[0]==1 && nums.size()==1)
-        // return true;
-        
-
-        // for(int i=0;i<nums.size()-1;i++){
-        //     int jump=nums[i];
-        //   while(jump){
-        //     int j=i;
-        //       j++;
-            
-        //     if(j==nums.size()-1 && jump==1)
-        //     return true;
-          
-        //     jump--;
-        //   }
-        // }
-        // return false;
-        int n=nums.size();
-        int reachable=0;
-        for(int i=0;i<n;i++){
-            if(reachable<i)
-                return false;
-            reachable=max(reachable,i+nums[i]);
-        }
-        return true;
-    }
-};
